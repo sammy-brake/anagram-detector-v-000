2949dc0a932b3cf08320e354505ae7cc1b9ef58c
Samantha brake <sammy.brake@gmail.com>
2018-09-18 19:46:51 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram 
+  attr_accessor :word 
+  @@anagram_array = []
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(array)
+    
+    array.collect do |test|
+     if  word.split.sort == test.split.sort 
+      @@anagram_array << test 
+     end 
+    end 
+    @@anagram_array
+  end 
+end 
